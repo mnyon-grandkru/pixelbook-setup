0091b9f103320bf5db7da6bf4ae32fd5ecfe8ee3
Mark Nyon <me@mnyon.com>
2021-03-05 17:50:17 -0500

    Install Oh My ZSH #4

---
+++ src/install_oh_my_zsh.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# This assumes that the zsh package has been installed.
+
+# Oh My ZSH install. Requires an input to confirm
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
