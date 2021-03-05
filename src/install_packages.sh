f11427e614545986436f6f495d5afa662caf65a1
Mark Nyon <me@mnyon.com>
2021-03-05 17:49:07 -0500

    Install debian packages #5

---
+++ src/install_packages.sh
@@ -0,0 +1,4 @@
+# Install packages needed to run certain software
+sudo apt-get install libdbus-glib-1-2       # Requirement for FireFox
+sudo apt-get install emacs                  # because, emacs
+sudo apt-get install zsh                    # Better shell
